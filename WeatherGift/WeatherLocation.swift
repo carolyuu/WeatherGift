f97843cb8fe6e3af3a4a3b3d15653b990324a5b9
Xiaotong <yuafq@bc.edu>
2022-03-14 13:27:41 -0400

    with .gitignore

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Carol Yu on 3/14/22.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
